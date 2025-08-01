c7c43c761e58157d109e245ada96fcc058c7cd3e
Dan Navarro <dannavs@yahoo.com>
2025-08-01 13:05:11 -0700

    flask added

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import yfinance as yf
 
-# Download historical data for a stock
-msft = yf.Ticker("MSFT")
-msft_data = msft.history(period="max")
 
-# Display the downloaded data
-print(msft_data.head())
+def the_msft():
+    msft = yf.Ticker("MSFT")
+    msft_data = msft.history(period="max")
+    print(msft_data.head(), end="\n")
+
+def the_sdiv():
+    sdiv = yf.Ticker("SDIV")
+    sdiv_data = sdiv.history(period="max")
+    print(sdiv_data.head())
